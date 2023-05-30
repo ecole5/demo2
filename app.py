907c13cbc61dc53723394cd245363a4e8b0954c5
Evan Cole <evanc@activestate.com>
2023-05-30 12:01:29 -0400

    Moving to tensorflow v2

--- app.py
+++ app.py
@@ -33,13 +33,16 @@ from flask import Flask, jsonify, request
 import numpy as np
 import tensorflow as tf
 
+#Disable eager execution to make sessions work
+tf.compat.v1.disable_eager_execution()
+
 app = Flask(__name__)
 
 def load_graph(model_file):
   graph = tf.Graph()
-  graph_def = tf.GraphDef()
+  graph_def = tf.compat.v1.GraphDef() 
 
-  with tf.gfile.GFile(model_file, "rb") as f:
+  with tf.compat.v2.io.gfile.GFile(model_file, "rb") as f:
     graph_def.ParseFromString(f.read())
   with graph.as_default():
     tf.import_graph_def(graph_def)
@@ -50,21 +53,21 @@ def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
 				input_mean=0, input_std=255):
   input_name = "file_reader"
   output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
+  file_reader =  tf.io.read_file(file_name, input_name)
   image_reader = tf.image.decode_image(file_reader, channels = 3,
       name='image_reader')
   float_caster = tf.cast(image_reader, tf.float32)
   dims_expander = tf.expand_dims(float_caster, 0)
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+  resized = tf.compat.v1.image.resize_bilinear(dims_expander, [input_height, input_width])
   normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
+  sess = tf.compat.v1.Session()
   result = sess.run(normalized)
 
   return result
 
 def load_labels(label_file):
   label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  proto_as_ascii_lines = tf.compat.v2.io.gfile.GFile(label_file).readlines()
   for l in proto_as_ascii_lines:
     label.append(l.rstrip())
   return label
@@ -79,7 +82,7 @@ def classify():
                                   input_mean=input_mean,
                                   input_std=input_std)
         
-    with tf.Session(graph=graph) as sess:
+    with tf.compat.v1.Session(graph=graph) as sess:
         start = time.time()
         results = sess.run(output_operation.outputs[0],
                       {input_operation.outputs[0]: t})
